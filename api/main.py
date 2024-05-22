2684fa7e68b52334068a2d883d7fac24b8119162
pulcovamon <pulcovamon@gmail.com>
2024-05-22 20:17:52 +0200

    change api dockerfile to run python main.py

--- api/main.py
+++ api/main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 app.include_router(endpoints.app, prefix="/air_pollution_api")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5050)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
