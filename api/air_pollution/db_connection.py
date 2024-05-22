b127a2e883c352a42d05528501a4489c0b4f4b32
pulcovamon <pulcovamon@gmail.com>
2024-05-22 21:13:54 +0200

    change root to user and setup db url

--- api/air_pollution/db_connection.py
+++ api/air_pollution/db_connection.py
@@ -1,9 +1,8 @@
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
 
-DATABASE_URL = 'sqlite:///./db/air_quality.db'
+DATABASE_URL = "mysql:///?User=user&Password=pass&Database=AirQuality&Server=myServer&Port=3306"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
 Base = declarative_base()
 
 def get_db():
--- data_processing/air_pollution/db_connection.py
+++ data_processing/air_pollution/db_connection.py
@@ -1,4 +1,4 @@
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = 'sqlite:///./db/air_quality.db'
+DATABASE_URL = "mysql:///?User=root&Password=pass&Database=AirQuality&Server=myServer&Port=3306"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
