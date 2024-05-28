5827cd99f80a72a2d04d86d96a4bd151cfde3c25
pulcovamon <pulcovamon@gmail.com>
2024-05-29 00:36:49 +0200

    add short introduction what is AQI

--- frontend/air-pollution/src/AQI.js
+++ frontend/air-pollution/src/AQI.js
@@ -20,6 +20,18 @@ export default function AQI({ selectedCity }) {
       }, [selectedCity]);
     return (
         <div>
+            <p>
+              The Air Quality Index (<b>AQI</b>) is used for reporting daily air quality.
+              It tells you how clean or polluted your air is, and what associated health effects might be a concern for you.
+              The AQI focuses on health effects you may experience within a few hours or days after breathing polluted air.
+              EPA calculates the AQI for five major air pollutants regulated by the Clean Air Act:
+              ground-level ozone, particle pollution (also known as particulate matter), carbon monoxide, sulfur dioxide, and nitrogen dioxide.
+              For each of these pollutants, EPA has established national air quality standards to protect public health.
+              Ground-level ozone and airborne particles are the two pollutants that pose the greatest threat to human health in this country.
+              <br />
+              <b>Source: <a href="https://www.weather.gov/safety/airquality-aqindex">National Weather Service</a></b>
+            </p>
+            <br />
             <Chart data={airQualityIndex} title={ "Air Quality Index" } />
         </div>
     );
