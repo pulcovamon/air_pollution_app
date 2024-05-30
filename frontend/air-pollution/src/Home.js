32e76e13845d479fb8275304c19464af2336215f
pulcovamon <pulcovamon@gmail.com>
2024-05-30 10:42:27 +0200

    change h1 to h3

--- frontend/air-pollution/src/Home.js
+++ frontend/air-pollution/src/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
     return (
         <div className="home">
-            <h1>Welcome to Air Quality Monitoring App</h1>
+            <h3>Welcome to Air Quality Monitoring App</h3>
             <p>
                 This application provides information about air quality in selected cities.
                 Origin of data is <a href="https://openweathermap.org/api/air-pollution">Open Weather - Air Pollution API</a>.
